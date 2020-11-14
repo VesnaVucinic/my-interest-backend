f83265752d39126fd2a2dafc1dcf9f9536ceecc1
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-11-14 23:42:22 +0000

    add login and logout actions

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,31 @@
+class Api::V1::SessionsController < ApplicationController
+    def create
+        @user = User.find_by(email: params[:user][:email])
+    
+        if @user && @user.authenticate(params[:user][:password])
+          session[:user_id] = @user.id
+          render json: UserSerializer.new(@user), status: :ok
+        else
+          render json: {
+            error: "Invalid Credentials"
+          }
+        end
+    end
+    
+    def get_current_user
+        if logged_in?
+          render json: UserSerializer.new(current_user)
+        else
+          render json: {
+            error: "No one logged in"
+          }
+        end
+    end
+    
+    def destroy
+        session.clear
+        render json: {
+          notice: "successfully logged out"
+        }, status: :ok
+    end 
+end
