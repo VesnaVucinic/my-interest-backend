229cdcb0509ab65b17514d312c504806e3d17cd4
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-11-22 00:03:22 +0000

    implement JWT

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,24 @@
 class ApplicationController < ActionController::API
-  include ActionController::Cookies
   
     def current_user
-        @current_user ||= User.find_by(id: session[:user_id])
+        begin
+        @current_user ||= User.find(decode_token_and_get_user_id)
+        rescue
+        return nil
+        end
     end
     
     def logged_in?
         # byebug
         !!current_user
+    end
+
+    def generate_token(payload)
+        JWT.encode(payload, ENV['JWT_TOKEN_SECRET'])
+    end
+    
+    def decode_token_and_get_user_id
+        JWT.decode(request.headers["Authorization"], ENV['JWT_TOKEN_SECRET'])[0]["id"]
     end
     
 end
