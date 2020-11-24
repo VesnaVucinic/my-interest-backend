4d9e79e022c098460aa74a6e9e279bc33ee27514
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-11-24 20:37:37 +0000

    add auto_login route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   post "/api/v1/signup", to: "api/v1/users#create"
   delete "/api/v1/logout", to: "api/v1/sessions#destroy"
   get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
+  get '/api/v1/auto-login', to: "session#auto_login"
+ 
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :create]
